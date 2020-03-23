a422ffdc0c9345b94cf4c78b61a5bcc41510cee7
unknown <aryeong22@hanail.net>
2020-03-23 16:10:51 +0900

    200323

---
+++ array03/ArrayEx02.java
@@ -0,0 +1,20 @@
+package array03;
+
+/**
+ * 
+ * @author Cos
+ *for문은 조건을 검사한다. 
+ *증가는 스택이 끝날때!!
+ *초기값은 최초에만 실행된다. 이후에는 조건만 검증하고 스택이 끝나면 증가! 
+ */
+
+public class ArrayEx02 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
